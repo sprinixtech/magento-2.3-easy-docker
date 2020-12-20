0073ab53af23640a3c0d853e661f147d8a844a6e
Gulshan Maurya <gulshan.4dream@gmail.com>
2020-12-20 22:06:26 +0530

    Docker for Magento 2.3

--- source/index.php
+++ source/index.php
@@ -1,4 +1,4 @@
 <h1> It Works!! </h1>
 <h2> I don't mind if you want to say thanks :) : <a href="mailto:gulshan.4dream@gmail.com">Gulshan Maurya</a>.</h2>
 
-<h3>Ohh! Don't mention it.</h3>
+<h3>Hey!! I understand that.</h3>
